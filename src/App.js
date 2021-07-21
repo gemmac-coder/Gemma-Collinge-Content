9f19d92d3bbc820be2e330bdf9c692922da9c093
gemmacollinge <gemmacollinge_71@hotmail.com>
2021-07-21 18:16:41 +0100

    infinite scroll in

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import InfiniteScroll from "./components/infiniteScroll.js";
+
+function App() {
+  return (
+    <div className="App">
+      <InfiniteScroll />
+    </div>
+  );
+}
+
+export default App;
