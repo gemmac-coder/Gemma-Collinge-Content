1482462383f9eee89b76735c3f9ac57f5cf5f97f
gemmacollinge <gemmacollinge_71@hotmail.com>
2021-07-23 23:08:22 +0100

    grid system

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import "../index.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 // import { Ionicons } from "react-icons/io";
 // import { FaBeer } from "react-icons/fa";
 
@@ -8,8 +11,23 @@ class Intro extends Component {
     return (
       <div id="Intro-Container">
         <div id="Welcome-Container">
-          <h1 className="neon-heading">WELCOME</h1>
-          <h2> I'm Gemma, Marketer, Writer, Full-Stack Developer.</h2>
+          <Container>
+            <Row>
+              <Col sm={12}>
+                <h1 className="neon-heading">WELCOME</h1>
+              </Col>
+            </Row>
+          </Container>
+          <Container>
+            <Row>
+              <Col sm={12}>
+                <h2 className="about-h2">
+                  {" "}
+                  I'm Gemma, Marketer, Writer, Full-Stack Developer.
+                </h2>
+              </Col>
+            </Row>
+          </Container>
         </div>
       </div>
     );
