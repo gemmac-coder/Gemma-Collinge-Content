2aaacd52d64bd9d1090265f133f7dc71dc79c8e5
gemmacollinge <gemmacollinge_71@hotmail.com>
2021-07-25 16:39:14 +0100

    animated intro screen in

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./components/Navbar.js";
-import Intro from "./components/Intro.js";
+// import "./components/Main.js";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-function App() {
+const Header = (props) => {
   return (
-    <div className="Container">
-      <Navbar />
-      <Intro />
-    </div>
+    <header id="welcome-section">
+      <div className="container">
+        <h1>
+          <span className="line">Hi, I'm Gemma.</span>
+          <br></br>
+        </h1>
+      </div>
+    </header>
   );
+};
+
+class App extends React.Component {
+  // state = {
+  //   menuState: false,
+  // };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+      </React.Fragment>
+    );
+  }
+
+  componentDidMount() {
+    const header = document.querySelector("#welcome-section");
+
+    window.onscroll = () => {
+      let scrollPos =
+        document.documentElement.scrollTop || document.body.scrollTop;
+
+      if (scrollPos - 100 <= window.innerHeight)
+        header.style.visibility =
+          header.style.visibility === "hidden" && "visible";
+      else header.style.visibility = "hidden";
+    };
+  }
 }
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,8 @@ class Navbar extends Component {
   render() {
     return (
       <div id="Navbar">
-        <div id="Github-icon-container">Git</div>
-        <div id="Linkedin-icon-container">LI</div>
+        <div id="Github-icon-container">GitHub</div>
+        <div id="Linkedin-icon-container">Linkedin</div>
       </div>
     );
   }
